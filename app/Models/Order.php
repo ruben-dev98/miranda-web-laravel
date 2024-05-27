e92d6b3266f9ab4cc35a1070d24079ebe79588b1
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-05-27 18:52:45 +0200

    ADD: order model, controller and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['room_id',
+    'user_id',
+    'type',
+    'description'];
+
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
